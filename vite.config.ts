d7667be7d1b2dd8782b79033386ef5a511d6d8c3
alexsad95 <aleksandrsadchikov.17@gmail.com>
2025-09-27 23:27:42 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
+  base: process.env.NODE_ENV === 'production' ? '/miniframework-example/' : '/',
   publicDir: 'public',
   build: {
     outDir: 'dist',
